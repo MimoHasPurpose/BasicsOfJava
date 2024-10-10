1ab350ed473d4c9f84de873a7f30385a5e8addf8
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-10 15:21:30 +0530

    added 2 files

---
+++ Maths.java
@@ -0,0 +1,10 @@
+
+public class Maths {
+    public static void main(String[] args){
+        double x=3.14;
+        double y=-10;
+        double z=Math.abs(y);
+
+        System.out.println(z);
+    }
+}
