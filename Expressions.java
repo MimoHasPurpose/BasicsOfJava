981a358b6de7b6cd41fd4e1bf2c2bb2671126f93
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-10 15:04:59 +0530

    added 2 files

--- Expressions.java
+++ Expressions.java
@@ -4,6 +4,9 @@ public class Expressions {
     public static void main(String[] args){
 
         int friends =10;
+
+        friends=friends%3;
+        friends+=1;
         System.out.println("friends: "+friends);
     }
 }
