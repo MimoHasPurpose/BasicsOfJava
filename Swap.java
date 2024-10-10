05db421d452b2d599b16934de793dacd14d95da1
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-10 11:57:34 +0530

    added 2 files

---
+++ Swap.java
@@ -0,0 +1,15 @@
+
+public class Swap {
+    public static void main(String [] args)
+    {
+        String x="hello";
+        String y="Kool-Aid";
+        String temp;
+
+        temp=x;
+        x=y;
+        y=temp;
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+    }
+}
