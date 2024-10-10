a23cd272daae4051350b6fdf9307af1a3743e961
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-10 16:11:29 +0530

    added a file

---
+++ Switchs.java
@@ -0,0 +1,11 @@
+public class Switchs {
+
+    public static void main(String[] args){
+        String day="Monday";
+        switch(day){
+            case "Monday": System.out.println("It is Monday");
+            break;
+            default: System.out.println("That is not a day");
+        }
+    }
+}
