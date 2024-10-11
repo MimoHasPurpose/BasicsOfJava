4a6faf52f4afc7ee320e079c8ba2c952e2b64924
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-11 10:54:00 +0530

    added a file

---
+++ WrappersClass.java
@@ -0,0 +1,14 @@
+public class WrappersClass {
+    public static void main(String[] args){
+
+        Boolean a=true;
+        Character b='@';
+        Integer c=123;
+        Double d=3.14;
+        String e="Bro";
+
+        if(a==true){
+            System.out.println("True");
+        }
+    }
+}
