e53979deecbce247efda61dbdc2923c811d620cc
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-12 10:25:17 +0530

    added a file

---
+++ OverloadedFunctions.java
@@ -0,0 +1,16 @@
+public class OverloadedFunctions {
+    public static void main(String[] args){
+        int x=add(1,2);
+        System.out.println(x);
+    }
+
+    static int add(int a,int b){
+        return a+b;
+    }
+    static int add(int a, int b, int c){
+        return a+b+c;
+    }
+    static int add(int a,int b, int c, int d){
+        return a+b+c+d;
+    }
+}
