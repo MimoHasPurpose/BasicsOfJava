40d7ae223e8616e66b88f055b5d67243047eb2e8
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-12 10:08:13 +0530

    added a file

---
+++ Methods.java
@@ -0,0 +1,12 @@
+public class Methods {
+    public static void main(String[] args){
+
+        String name="Bro";
+        int age=21;
+        hello(name,age);
+    }
+    static void hello(String name,int age){
+        System.out.println("Hello");
+        System.out.println("You are: "+age);
+    }
+}
---
+++ MethodsTwo.java
@@ -0,0 +1,15 @@
+public class MethodsTwo {
+
+    public static void main(String[] args){
+        int x=3;
+        int y=4;
+        int z= add(x,y);
+        System.out.println(z);
+
+    }
+    static int add(int x,int y){
+        int z=x+y;
+        return z;
+    }
+
+}
