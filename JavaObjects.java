50c232830d28a349d55d96b671ed2c887f38c175
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-12 11:22:46 +0530

    added a file

---
+++ JavaObjects.java
@@ -0,0 +1,10 @@
+public class JavaObjects{
+    public static void main(String[] args){
+        Car myCar1=new Car();
+        Car myCar2=new Car();
+        System.out.println(myCar1.make);
+        System.out.println(myCar2.make);
+
+
+    }
+}
