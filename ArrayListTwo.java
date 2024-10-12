f675faac7e299df917d319af2742e660a189cf74
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-12 08:28:25 +0530

    added a file

--- ArrayListTwo.java
+++ ArrayListTwo.java
@@ -1,6 +1,9 @@
 import java.util.*;
 public class ArrayListTwo {
     public static void main(String[] args){
+
+
+        ArrayList<ArrayList<String>> groceryList= new ArrayList();
         ArrayList<String> bakeryList=new ArrayList();
         bakeryList.add("pasta");
         bakeryList.add("garlic bread");
@@ -11,7 +14,13 @@ public class ArrayListTwo {
         produceList.add("zucchini");
         produceList.add("peppers");
 
-        System.out.println(bakeryList.get(0));
+        ArrayList<String> drinksList=new ArrayList();
+        drinksList.add("soda");
+        drinksList.add("coffee");
 
+        groceryList.add(bakeryList);
+        groceryList.add(produceList);
+        groceryList.add(drinksList);
+        System.out.println(groceryList);
     }
 }
