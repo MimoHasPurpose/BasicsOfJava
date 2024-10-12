7ccae19ac98fd9462c2ff0bac202061822f57991
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-12 15:52:07 +0530

    added a file

--- OverloadedConstructors/Main.java
+++ OverloadedConstructors/Main.java
@@ -1,4 +1,16 @@
 package OverloadedConstructors;
 
 public class Main {
+    public static void main(String[] args){
+        Pizza pizza=new Pizza("thicc crust","tomato","mozzerella","pepperoni");
+        System.out.println("Ingredients of your pizza: ");
+        System.out.println(pizza.bread);
+        System.out.println(pizza.sauce);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.topping);
+        Pizza pizza2=new Pizza("thicc crust","tomato","mozzerella");
+
+
+
+    }
 }
