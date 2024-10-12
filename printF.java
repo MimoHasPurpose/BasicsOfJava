4647110afb4816015315d70e47b71631083dcd47
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-12 11:01:07 +0530

    added a file

---
+++ printF.java
@@ -0,0 +1,18 @@
+public class printF {
+    public static void main(String [] args){
+//        System.out.printf("This is a format string %d", 123);
+//    boolean myBoolean=true;
+//    char myChar='@';
+//    String myString="Bro";
+//    int myInt=50;
+//    double myDouble=1000;
+//    System.out.printf("%b ",myBoolean);
+//    System.out.printf("%c ",myChar);
+//    System.out.printf("%s ",myString);
+//    System.out.printf("%d ",myInt);
+//    System.out.printf("%f",myDouble);
+      String myString="FromTheOtherSide";
+      System.out.printf("hello %10s",myString);
+
+    }
+}
