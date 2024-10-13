c7f5b8a59f1b66ad8b6f32961408c40c27d2fbcc
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-10-13 17:11:25 +0530

    some changes

---
+++ AbstractionsInClass/Car.java
@@ -0,0 +1,10 @@
+package AbstractionsInClass;
+
+public class Car extends Vehicle {
+
+
+    @Override
+    void go(){
+        System.out.println("The driver");
+    }
+}
---
+++ AbstractionsInClass/Vehicle.java
@@ -0,0 +1,7 @@
+package AbstractionsInClass;
+
+public abstract class Vehicle {
+
+    abstract void go();
+
+}
