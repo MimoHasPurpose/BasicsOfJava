fb097dfde28b9eeb956b9bb2d75b4a371827d509
Sanjay <77540699+MimoHasPurpose@users.noreply.github.com>
2025-04-18 20:17:08 +0530

    Update Predator.java

--- JavaInterface/Predator.java
+++ JavaInterface/Predator.java
@@ -1,4 +1,7 @@
-package JavaInterface;
+// Another way to achieve abstraction in Java, is with interfaces.
+
+// An interface is a completely "abstract class" that is used to group related methods with empty bodies:
+// package JavaInterface;
 
 public interface Predator {
     void hunt();
